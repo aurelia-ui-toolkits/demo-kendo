969b961de5d45f77c492e7f29670366701d566e0
Jeroen <Jeroenvinke17@gmail.com>
2016-02-11 12:38:44 +0100

    Update 2016-02-11T11:38:32.329Z

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -16,6 +16,7 @@ export class NavBar {
     this.dialog = jQuery(this.themeSelector).kendoWindow({
       width: '800px',
       title: 'Themes',
+      visible: false,
       actions: [
         'Pin',
         'Minimize',
