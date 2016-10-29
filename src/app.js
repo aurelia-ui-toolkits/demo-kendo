ca5cd146f69ac53db8487b8354f875747bf6a0e0
Jeroen Vinke <JeroenVinke@users.noreply.github.com>
2016-10-29 22:59:07 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
     config.title = 'Kendo UI Samples';
 
     config.map([
-      { name: 'about',            route: 'about',             moduleId: 'about/about',                 title: 'About123' },
+      { name: 'about',            route: 'about',             moduleId: 'about/about',                 title: 'About' },
       { name: 'catalog-index',    route: 'catalog-index',     moduleId: 'catalog-index/controls',      title: 'Catalog-index'},
       { name: 'documentation',    route: 'documentation',     moduleId: 'documentation/documentation', title: 'Documentation'},
       { name: 'help',             route: 'help',              moduleId: 'help/index',                  title: 'Help' },
